87c3fc4d24ea014b187059449cf3662dadd6f0a3
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2022-02-27 12:37:12 +0800

    Update README.md

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌥⇧R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import time
 from typing import *
 import requests as requests
